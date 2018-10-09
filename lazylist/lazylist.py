6c6525a3dce9b8dbdd05b2dd73c493a68041f623
miruka <miruka@disroot.org>
2018-10-09 08:59:06 -0400

    Bug fixes, readability, package __init__
    - Fixed methods like .append() (itertools.chain not imported)
    - Fixed slicing without specifying all start/end/step, e.g. ll[:3]
    - Fixed accessing list element once it's unlazyfied, e.g. ll[1]; ll[0]
    
    - Make code readable with blank lines
    - Do not show "..." at the end of the repr if the list is fully loaded
    
    - Put file in package dir with a __init__, import LazyList globally

--- lazylist/lazylist.py
+++ lazylist/lazylist.py
@@ -1,64 +1,97 @@
-#!/usr/bin/env python3
-
 import collections.abc
-from itertools import islice
+from itertools import chain, islice
 
-__all__ = ['LazyList']
 
 class LazyList(collections.abc.MutableSequence):
     def __init__(self, iterable):
-        self._nonlazy, self._lazy = [], iter(iterable)
+        self._nonlazy = []
+        self._lazy    = iter(iterable)
+
+        self.is_fully_loaded = False
+
+
     def __len__(self):
         self._delazify()
         return len(self._nonlazy)
+
+
     def _delazify(self, index=None):
         if index is None:
             self._nonlazy.extend(self._lazy)
+            self.is_fully_loaded = True
             return
+
         if isinstance(index, slice):
-            if index.start < 0 or index.stop < 0:
+            if (index.start or 0) < 0 or (index.stop or -1) < 0:
                 self._delazify()
                 return
-            index = range(index.start, index.stop, index.step)[-1]
+
+            index = range(index.start or 0, index.stop, index.step or 1)[-1]
+
         if index < 0:
             self._delazify()
             return
-        self._nonlazy.extend(islice(self._lazy, index - len(self._nonlazy) + 1))
+
+        islice_start = index - len(self._nonlazy) + 1
+
+        if islice_start < 0:
+            # elements are already unlazyfied
+            return
+
+        self._nonlazy.extend(
+            islice(self._lazy, islice_start)
+        )
+
+
     def __getitem__(self, index):
         self._delazify(index)
         return self._nonlazy[index]
+
+
     def __delitem__(self, index):
         self._delazify(index)
         del self._nonlazy[index]
+
+
     def __setitem__(self, index, value):
         self._delazify(index)
         self._nonlazy[index] = value
+
+
     def insert(self, index, value):
         if index:
             self._delazify(index-1)
         self._nonlazy.insert(index, value)
+
+
     def __iter__(self):
         yield from self._nonlazy
+
         for value in self._lazy:
             self._nonlazy.append(value)
             yield value
+
+
     def append(self, value):
         self._lazy = chain(self._lazy, (value,))
+
+
     def extend(self, values):
         self._lazy = chain(self._lazy, values)
+
+
     def clear(self):
         self._nonlazy, self._lazy = [], iter(())
+
+
     def __str__(self):
-        return '[{}]'.format(', '.join(list(map(repr, self._nonlazy)) + ['...']))
+        reprs = list(map(repr, self._nonlazy))
+
+        if not self.is_fully_loaded:
+            reprs.append("...")
+
+        return "[%s]" % ", ".join(reprs)
+
+
     def __repr__(self):
-        return 'LazyList({})'.format(self)
-
-if __name__ == '__main__':
-    import sys
-    ll = LazyList(sys.argv)
-    print(ll)
-    print(ll[1])
-    print(ll)
-    print(ll[-3:-2])
-    print(ll)
-
+        return f"LazyList({self})"
